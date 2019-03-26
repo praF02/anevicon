d2ac6883195494799dabc455bcd8acc9076ca56a
Temirkhan Myrzamadi <gymmasssorla@gmail.com>
2019-03-27 02:08:45 +0600

    Fix the anevicon project description line

--- anevicon_core/src/lib.rs
+++ anevicon_core/src/lib.rs
@@ -1,4 +1,4 @@
-// anevicon: The most powerful UDP-based load generator, written in Rust.
+// anevicon: A high-performant UDP-based load generator, written in Rust.
 // Copyright (C) 2019  Temirkhan Myrzamadi <gymmasssorla@gmail.com>
 //
 // This program is free software: you can redistribute it and/or modify
