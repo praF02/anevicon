76f8f3e58cf1b639a61b939f938f19c70af52375
Temirkhan Myrzamadi <gymmasssorla@gmail.com>
2019-05-03 12:38:56 +0600

    Remove all the AppImage configuration files
    I examined that Anevicon is already a statically-compiled program without any external dependencies, so the AppImage format is exhaustive here.

--- appimage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This script simply generates an .AppImage file inside the anevicon.AppDir
-# directory. Usage: ./appimage.sh
-
-cargo build --release
-cp target/release/anevicon anevicon.AppDir/anevicon
-cd anevicon.AppDir
-./appimagetool-x86_64.AppImage .
