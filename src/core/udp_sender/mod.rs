972884817935479a489d91e6cd68af722078cbea
Temirkhan Myrzamadi <gymmasssorla@gmail.com>
2019-07-19 13:20:34 +0600

    Fix the errors (udp_sender/mod.rs)

--- src/core/udp_sender/mod.rs
+++ src/core/udp_sender/mod.rs
@@ -18,7 +18,10 @@
 
 use std::io;
 use std::io::IoSlice;
+use std::net::SocketAddr;
 use std::num::NonZeroUsize;
+
+use nix::sys::socket::{InetAddr, IpAddr, SockAddr};
 
 use sendmmsg::sendmmsg;
 
@@ -55,38 +58,47 @@ impl<'a> UdpSender<'a> {
     /// # Panics
     /// This associated function panics if your OS cannot create a raw IPv4/IPv6
     /// socket or correctly set one of the socket options.
-    pub fn new(is_ipv4: bool, capacity: NonZeroUsize, config: &SocketsConfig) -> Self {
-        UdpSender {
-            fd: {
-                let fd = unsafe {
-                    libc::socket(
-                        if is_ipv4 {
-                            libc::AF_INET
-                        } else {
-                            libc::AF_INET6
-                        },
-                        libc::SOCK_RAW,
-                        libc::IPPROTO_RAW,
-                    )
-                };
+    pub fn new(
+        dest: &SocketAddr,
+        capacity: NonZeroUsize,
+        config: &SocketsConfig,
+    ) -> nix::Result<Self> {
+        let dest = InetAddr::from_std(dest);
+        let is_ipv4 = match dest.ip() {
+            IpAddr::V4(_) => true,
+            IpAddr::V6(_) => false,
+        };
 
-                if fd == -1 {
-                    panic!(
-                        "Failed to create a raw socket >>> {}",
-                        io::Error::last_os_error()
-                    );
+        let fd = unsafe {
+            libc::socket(
+                if is_ipv4 {
+                    libc::AF_INET
                 } else {
-                    helpers::set_socket_options(fd, config);
-                    fd
-                }
-            },
+                    libc::AF_INET6
+                },
+                libc::SOCK_RAW,
+                libc::IPPROTO_RAW,
+            )
+        };
 
+        if fd == -1 {
+            panic!(
+                "Failed to create a raw socket >>> {}",
+                io::Error::last_os_error()
+            );
+        }
+
+        nix::sys::socket::connect(fd, &SockAddr::Inet(dest))?;
+        helpers::set_socket_options(fd, config);
+
+        Ok(UdpSender {
+            fd,
             buffer: {
                 let mut packets = Vec::new();
                 packets.reserve_exact(capacity.get());
                 packets
             },
-        }
+        })
     }
 
     pub fn supply(
@@ -157,15 +169,17 @@ mod helpers {
 
 #[cfg(test)]
 mod tests {
+    use std::convert::TryInto;
     use std::io::IoSlice;
-    use std::net::UdpSocket;
+    use std::net::{Ipv4Addr, UdpSocket};
     use std::ops::Deref;
     use std::os::unix::io::AsRawFd;
     use std::time::Duration;
 
-    use nix::sys::socket::getsockopt;
-    use nix::sys::socket::sockopt::{Broadcast, SendTimeout};
-    use nix::sys::time::{suseconds_t, time_t};
+    use pnet::packet::ip::IpNextHeaderProtocols;
+    use pnet::packet::ipv4::MutableIpv4Packet;
+    use pnet::packet::udp::MutableUdpPacket;
+    use pnet::packet::{Packet, PacketSize};
 
     use lazy_static::lazy_static;
 
@@ -179,15 +193,66 @@ mod tests {
             broadcast: false,
             send_timeout: Duration::from_secs(3),
         };
+        static ref UDP_SERVER: UdpSocket =
+            UdpSocket::bind("127.0.0.1:0").expect("Failed to setup UDP_SERVER");
+
+        static ref TEST_UDP_PACKET: MutableIpv4Packet<'static> = {
+            let payload = b"Our packet";
+
+            // Construct a UDP packet
+            let mut udp_packet =
+                MutableUdpPacket::owned(vec![0; UDP_HEADER_LENGTH + payload.len()]).unwrap();
+            udp_packet.set_source(181);
+            udp_packet.set_destination(UDP_SERVER.local_addr().unwrap().port());
+            udp_packet.set_length((UDP_HEADER_LENGTH + payload.len()).try_into().unwrap());
+            udp_packet.set_payload(payload.as_ref());
+            udp_packet.set_checksum(0);
+            udp_packet.set_checksum(pnet::packet::udp::ipv4_checksum_adv(
+                &udp_packet.to_immutable(),
+                payload.as_ref(),
+                &Ipv4Addr::new(127, 0, 0, 1),
+                &Ipv4Addr::new(127, 0, 0, 1),
+            ));
+
+            // Construct an IPv4 packet
+            let mut ipv4_packet =
+                MutableIpv4Packet::owned(vec![0; IPV4_HEADER_LENGTH + udp_packet.packet_size()])
+                    .unwrap();
+            ipv4_packet.set_version(4);
+            ipv4_packet.set_header_length((IPV4_HEADER_LENGTH / 4).try_into().unwrap());
+            ipv4_packet.set_dscp(0);
+            ipv4_packet.set_ecn(0);
+            ipv4_packet.set_total_length(
+                (IPV4_HEADER_LENGTH + UDP_HEADER_LENGTH + payload.len())
+                    .try_into()
+                    .unwrap(),
+            );
+            ipv4_packet.set_identification(0x123);
+            ipv4_packet.set_flags(0);
+            ipv4_packet.set_fragment_offset(0);
+            ipv4_packet.set_ttl(8);
+            ipv4_packet.set_next_level_protocol(IpNextHeaderProtocols::Udp);
+            ipv4_packet.set_source(Ipv4Addr::new(127, 0, 0, 1));
+            ipv4_packet.set_destination(Ipv4Addr::new(127, 0, 0, 1));
+            ipv4_packet.set_payload(udp_packet.packet());
+            ipv4_packet.set_checksum(0);
+            ipv4_packet.set_checksum(pnet::packet::ipv4::checksum(&ipv4_packet.to_immutable()));
+
+            ipv4_packet
+        };
     }
+
+    const IPV4_HEADER_LENGTH: usize = 20;
+    const UDP_HEADER_LENGTH: usize = 8;
 
     #[test]
     fn constructs_buffer() {
         let buffer = UdpSender::new(
-            true,
+            &UDP_SERVER.local_addr().unwrap(),
             NonZeroUsize::new(354).unwrap(),
             &DEFAULT_SOCKETS_CONFIG,
-        );
+        )
+        .expect("UdpSender::new(...) failed");
 
         assert_eq!(buffer.buffer.capacity(), 354);
         assert_eq!(buffer.buffer.len(), 0);
@@ -196,17 +261,25 @@ mod tests {
     #[test]
     fn test_packets_buffer() {
         let mut summary = TestSummary::default();
-        let mut buffer =
-            UdpSender::new(true, NonZeroUsize::new(4).unwrap(), &DEFAULT_SOCKETS_CONFIG);
+        let mut buffer = UdpSender::new(
+            &UDP_SERVER.local_addr().unwrap(),
+            NonZeroUsize::new(4).unwrap(),
+            &DEFAULT_SOCKETS_CONFIG,
+        )
+        .expect("UdpSender::new(...) failed");
+        ;
 
         let check = |buffer: &UdpSender| {
             assert_eq!(buffer.buffer.capacity(), 4);
-            assert_eq!(buffer.buffer.last().unwrap().1.deref(), b"Our packet");
+            assert_eq!(
+                buffer.buffer.last().unwrap().1.deref(),
+                TEST_UDP_PACKET.packet()
+            );
         };
 
         let mut supply = |buffer: &mut UdpSender| {
             buffer
-                .supply(&mut summary, (0, IoSlice::new(b"Our packet")))
+                .supply(&mut summary, (0, IoSlice::new(TEST_UDP_PACKET.packet())))
                 .expect("buffer.supply() failed");
         };
 
